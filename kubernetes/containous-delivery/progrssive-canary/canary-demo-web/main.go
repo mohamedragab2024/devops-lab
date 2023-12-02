88c08356b376c61bb604908ab297f877bf147905
Mohamed Ragab <mragab6010@gmail.com>
2023-12-02 08:36:01 +0200

    add new version

--- kubernetes/containous-delivery/progrssive-canary/canary-demo-web/main.go
+++ kubernetes/containous-delivery/progrssive-canary/canary-demo-web/main.go
@@ -4,20 +4,25 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if val, err := strconv.ParseBool(r.URL.Query().Get("error")); err != nil && val == true {
+			http.Error(w, "An error occurred", 500)
+			return
+		}
 		htmlContent := `
     <!DOCTYPE html>
     <html>
     <head>
-        <title>Green deployment</title>
+        <title>Blue deployment</title>
     </head>
     <body style="background:blue">
         <h1>Simple Go web app!</h1>
-        <p>Welcome to green version</p>
+        <p>Welcome to blue version</p>
         
     </body>
     </html>
